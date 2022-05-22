d36d311f0f0a58029cc6e013cfbefb5813c503fb
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2022-05-22 23:45:49 +0100

    Make PoC support multiple Sockets

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,35 @@ use std::{
     net::{TcpListener, TcpStream},
 };
 
-fn main() -> Result<()> {
-    let listener = get_tcplistener();
+#[cfg(target_os = "wasi")]
+use wasi::fd_fdstat_get;
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => handle_connection(stream),
+#[cfg(target_os = "wasi")]
+use std::os::wasi::io::RawFd;
+
+fn main() -> Result<()> {
+    let listeners = get_tcplistener();
+    println!("{:?}", listeners);
+    let mut idx = 0usize;
+    loop {
+        let listener = &listeners[idx];
+
+        match listener.accept() {
+            Ok((stream, _)) => {
+                handle_connection(stream);
+            }
             Err(err) if err.kind() == ErrorKind::WouldBlock => {
-                sleep(time::Duration::from_millis(5))
+                sleep(time::Duration::from_micros(1))
             }
             Err(err) => panic!("{}", err),
         }
-    }
 
-    Ok(())
+        idx = if (idx + 1) == listeners.len() {
+            0
+        } else {
+            idx + 1
+        }
+    }
 }
 
 fn handle_connection(mut stream: TcpStream) {
@@ -34,7 +49,7 @@ fn handle_connection(mut stream: TcpStream) {
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
 
-fn get_tcplistener() -> TcpListener {
+fn get_tcplistener() -> Vec<TcpListener> {
     #[cfg(target_os = "wasi")]
     return get_tcplistener_wasi();
 
@@ -43,14 +58,37 @@ fn get_tcplistener() -> TcpListener {
 }
 
 #[cfg(target_os = "wasi")]
-fn get_tcplistener_wasi() -> TcpListener {
-    use std::os::wasi::prelude::{FromRawFd, RawFd};
-
-    let sock: RawFd = 3i32;
-    unsafe { TcpListener::from_raw_fd(sock) }
+fn get_tcplistener_wasi() -> Vec<TcpListener> {
+    use std::os::wasi::prelude::FromRawFd;
+    enumerate_tcp_fd()
+        .into_iter()
+        .map(|raw_fd| unsafe { TcpListener::from_raw_fd(raw_fd) })
+        .collect()
 }
 
 #[cfg(not(target_os = "wasi"))]
-fn get_tcplistener_bind() -> TcpListener {
-    TcpListener::bind("127.0.0.1:25565").expect("Could not bind address")
+fn get_tcplistener_bind() -> Vec<TcpListener> {
+    let mut listeners = Vec::new();
+    listeners.push(TcpListener::bind("127.0.0.1:25565").expect("Could not bind address"));
+    listeners.push(TcpListener::bind("127.0.0.1:25566").expect("Could not bind address"));
+    listeners
+}
+
+#[cfg(target_os = "wasi")]
+
+fn enumerate_tcp_fd() -> Vec<RawFd> {
+    use wasi::FILETYPE_SOCKET_STREAM;
+
+    let mut stats = Vec::<RawFd>::new();
+    for i in 1u32..1000 {
+        if let Ok(fdstat) = unsafe { fd_fdstat_get(i) } {
+            if fdstat.fs_filetype == FILETYPE_SOCKET_STREAM {
+                stats.push(i as RawFd)
+            }
+        } else {
+            break;
+        }
+    }
+
+    stats
 }
