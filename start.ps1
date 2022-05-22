dd44eb89a55a30e8ff5fa8c6c046526630fff96d
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2022-05-22 23:48:17 +0100

    Add second port to run cmd

--- start.ps1
+++ start.ps1
@@ -6,6 +6,9 @@ param (
 	[Parameter()]
 	[String]
 	$Port = "25565"
+	[Parameter()]
+	[String]
+	$Port2 = "25566"
 )
 
-wasmtime --tcplisten=127.0.0.1:$Port .\target\wasm32-wasi\$Config\wasi-net-test.wasm
+wasmtime --tcplisten=127.0.0.1:$Port --tcplisten=127.0.0.1:$Port2 .\target\wasm32-wasi\$Config\wasi-net-test.wasm
